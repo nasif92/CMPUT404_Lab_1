cefebc13276d3074358f455536b217bbffbc2e42
pickabo <nhossain@ualberta.ca>
2022-01-13 21:14:42 -0700

    Adding everything

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -1,7 +1,13 @@
 import requests as rq
 
-print(rq.__version__)
+req_version = rq.__version__
 
 URL = "http://www.google.com"
 r = rq.get(url = URL)
-print(r)
+
+RAW_URL = "https://raw.githubusercontent.com/nasif92/CMPUT404_Lab_1/master/Lab%201/lab1.py"
+git_req = rq.get(url=RAW_URL)
+
+print("The request version is", req_version)
+print("The google homepage is\n" + r.text,"\n")
+print("The python script is\n" + git_req.text,"\n")
